739e86efed64c61079780d16a5405778eba2d544
jpsider <jsider@belaytech.com>
2018-07-17 21:20:33 -0400

    Added a function to pull function names from a large script. Found it on twitter. Will be super useful.

---
+++ Powershell/Random/Get-ScriptFunctionName.ps1
@@ -0,0 +1,8 @@
+function Get-ScriptFunctionName{
+    param(
+        [string]$Path,
+        [switch]$Recurse
+    )
+    
+    [Management.Automation.Language.Parser]::ParseFile((Resolve-Path $Path),[ref]$null,[ref]$null).FindAll({param($c)$c-is[Management.Automation.Language.FunctionDefinitionAst]},$Recurse).Name
+}
