eb5f0022ba0d2867dceb3802f9b50353fa196473
jpsider <jpsider@gmail.com>
2019-09-24 13:12:22 -0400

    added sample function files and unit tests.

---
+++ Powershell/Plaster/FullModuleTemplate/tests/private/Disable-SSLValidation.Tests.ps1
@@ -0,0 +1,11 @@
+$script:ModuleName = '<%= $PLASTER_PARAM_ModuleName %>'
+
+$here = (Split-Path -Parent $MyInvocation.MyCommand.Path) -replace 'tests', "$script:ModuleName"
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Disable-SSLValidation function for $moduleName" -Tags Build {
+    It "Should Return null." {
+        Disable-SSLValidation | Should be $true
+    }
+}
