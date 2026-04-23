5cb675d2af18cd44672ae278df0f79f89c96b695
jpsider <jpsider@gmail.com>
2026-04-23 16:28:44 -0500

    minor updates from demo day

--- VMUG/Connect_MN_2026/runContainer.ps1
+++ VMUG/Connect_MN_2026/runContainer.ps1
@@ -11,7 +11,7 @@ Install-Module -Name VCF.PowerCLI -Force -Confirm:0
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -ParticipateInCeip $false -Confirm:0
 
 # Connect to vCenter Simulator
-Connect-VIServer -server 172.30.160.1 -user u -password p
+Connect-VIServer -server 172.23.16.1 -user u -password p
 
 # List some things!
 Get-VM
